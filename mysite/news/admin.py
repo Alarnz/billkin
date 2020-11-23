57830b8db9436df8f6aa10252c32b518ec7e55d5
alarnz <a1079768843@163.com>
2020-11-23 16:05:24 +0800

    作业提交

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -4,3 +4,6 @@ from . import models
 
 admin.site.register(models.Article)
 admin.site.register(models.Reporter)
+
+#作业#
+admin.site.register(models.Student)
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Article
+
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {'year': year, 'article_list': a_list}
+    return render(request, 'news/year_archive.html', context)
+
+
+#作业提交#
+from .models import Student, Homework
+
+from django.views.generic.edit import CreateView
+
+class HomeworkCreate(CreateView):
+    model = Homework
+    template_name = 'homework_form.html'
+    fields = ['commit_date','headline','attach','remark', 'student']
